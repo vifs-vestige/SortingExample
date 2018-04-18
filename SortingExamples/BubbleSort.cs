7462a66fb56de72cbc6c42e6df782097ef591393
ShawnSwinsick <vifs888@gmail.com>
2018-04-18 13:36:48 -0600

    bubble sort done

--- SortingExamples/BubbleSort.cs
+++ SortingExamples/BubbleSort.cs
@@ -9,12 +9,22 @@ namespace SortingExamples
     class BubbleSort
     {
 
-        public static List<int> Sort(List<int>)
+        public static void Sort(List<int> list)
         {
+            Console.WriteLine("Bubble Sort");
+            Console.WriteLine("");
+            Program.PrintList(list);
+            for (int i = 0; i < list.Count; i++)
+            {
+                for (int x = 0; x < list.Count - 1; x++)
+                {
+                    if (list[x] > list[x + 1])
+                        Program.Swap(list, x, x + 1);
+                }
+            }
+            Program.PrintList(list);
+            Console.WriteLine("");
 
-
-
-            return null;
         }
     }
 }
--- SortingExamples/Program.cs
+++ SortingExamples/Program.cs
@@ -8,8 +8,49 @@ namespace SortingExamples
 {
     class Program
     {
+        private static Random RNG;
         static void Main(string[] args)
         {
+            RNG = new Random();
+            BubbleSort.Sort(GetList(10));
+            Console.ReadLine();
+        }
+
+        public static List<int> GetList(int size)
+        {
+            var temp = new List<int>();
+            for (int i = 0; i < size; i++)
+            {
+                temp.Add(RNG.Next(99));
+            }
+            return temp;
+        }
+
+        public static void Swap(List<int> list, int one, int two)
+        {
+            var temp = list[one];
+            list[one] = list[two];
+            list[two] = temp;
+            PrintList(list, one, two);
+        }
+
+        public static void PrintList(List<int> list, int one, int two)
+        {
+            for (int i = 0; i < list.Count; i++)
+            {
+                Console.BackgroundColor = ConsoleColor.Black;
+                if (i == one)
+                    Console.BackgroundColor = ConsoleColor.Red;
+                if (i == two)
+                    Console.BackgroundColor = ConsoleColor.Blue;
+                Console.Write(list[i] + " ");
+            }
+            Console.WriteLine("");
+        }
+
+        public static void PrintList(List<int> list)
+        {
+            Console.WriteLine(string.Join(" ", list));
         }
     }
 }
