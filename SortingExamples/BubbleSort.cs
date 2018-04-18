559caa85afd37392719307c1af8ae641fe0c0c1a
ShawnSwinsick <vifs888@gmail.com>
2018-04-18 13:16:07 -0600

    First check in

---
+++ SortingExamples/BubbleSort.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SortingExamples
+{
+    class BubbleSort
+    {
+
+        public static List<int> Sort(List<int>)
+        {
+
+
+
+            return null;
+        }
+    }
+}
---
+++ SortingExamples/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SortingExamples
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
